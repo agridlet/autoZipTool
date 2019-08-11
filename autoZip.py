3a9b3e9a5bf6c39a2689deba32c5f3507a30729c
Agrid <andrei.gridlet@gmail.com>
2019-08-11 13:52:14 +0200

    first commit

---
+++ autoZip.py
@@ -0,0 +1,52 @@
+import os
+import zipfile
+import shutil
+from tkinterLearning import folder_path_transit, folder_path_model_webgl
+
+print("fichier ouvert")
+
+extensionBin = ".bin"
+
+# adresse où va arriver ton export 3dsmax
+# RepoTransit = "F:/infographie/dev/autoZip/transit/"
+RepoTransit = "{}".format(folder_path_transit)
+# adresse de là où tu mets ton zip pour le webgl
+# ModelWebgl = "F:/infographie/dev/autoZip/target/"
+ModelWebgl = "{}".format(folder_path_model_webgl)
+# adresse où tu vas sauvegarder une copie de ton ZIP
+SaveDirectory = "F:/infographie/dev/autoZip/target_save/"
+
+# detecte tout les fichiers dans le dossier et les mets dans une liste
+listeFichier = os.listdir(RepoTransit)
+
+# recherche le fichier avec la bonne extension pour capturer son nom de fichier
+for listEntry in listeFichier:
+    if listEntry.endswith(extensionBin):
+        finalNameFile = listEntry.replace(extensionBin, "")
+        finalName_ZIP = finalNameFile + ".zip"
+        # print(listEntry)
+        # print(finalNameZIP)
+
+print(finalName_ZIP)
+
+# Creation du fichier zip avec le nom capturé au dessus
+myZipfile = zipfile.ZipFile("{}{}".format(RepoTransit, finalName_ZIP), mode='w', compression=zipfile.ZIP_DEFLATED)
+
+
+# Introduction des fichiers qui sont présents dans le directory à lintérieur du fichier zip
+for fichier2Save in listeFichier:
+    print(fichier2Save, "zipped in file")
+    myZipfile.write("{0}{1}".format(RepoTransit, fichier2Save), arcname=fichier2Save)
+
+myZipfile.close()
+
+print("--------------------------")
+
+# efface les fichiers qui ont servis à creer le zip afin de nettoyer le directory
+# for fichier2Save in listeFichier:
+#     print(fichier2Save, "cleaned from base directory")
+#     os.remove("U:/Sandbox/Andrei/python_script/test/{0}".format(fichier2Save))
+
+# Bouge le fichier zip dans sa location final et dans un dossier de sauvegarde
+shutil.copy("{}{}".format(RepoTransit, finalName_ZIP), SaveDirectory)
+shutil.move("{}{}".format(RepoTransit, finalName_ZIP), ModelWebgl)
---
+++ tkinterLearning.py
@@ -0,0 +1,65 @@
+from tkinter import *
+from tkinter import filedialog
+from subprocess import Popen
+
+def browse_button():
+    # Allow user to select a directory and store it in global var
+    # called folder_path
+    global folder_path_transit
+    global folder_path_model_webgl
+    filename = filedialog.askdirectory()
+    folder_path_transit.set(filename)
+    print(filename)
+
+    filename_webgl = filedialog.askdirectory()
+    folder_path_model_webgl.set(filename_webgl)
+    print(filename_webgl)
+
+def start():
+    import os
+#    os.system("python test.py")
+    Popen(["python", "autoZip.py"])
+
+# ma premiere fenetre
+window = Tk()
+folder_path_transit = StringVar()
+folder_path_model_webgl = StringVar()
+print(folder_path_transit)
+print(folder_path_model_webgl)
+
+lbl1 = Label(master=window, textvariable=folder_path_transit)
+lbl1.grid(row=0, column=1)
+
+lbl12 = Label(master=window, textvariable=folder_path_model_webgl)
+lbl12.grid(row=1, column=1)
+
+button2 = Button(text="transit directory", command=browse_button)
+button2.grid(row=0, column=3)
+
+button3 = Button(text="webgl directory", command=browse_button)
+button3.grid(row=1, column=3)
+
+start = Button(text="ZIP", command=start)
+start.grid(row=2, column=3)
+
+# personnaliser cette fenetre
+window.title("Auto Export Zip by GerbilDindon")
+# window.geometry("512x256")
+
+# ajout de text
+label_title_from_max = Label(window, text="Chemin d'accès vers le dossier d'export 3ds")
+# label_title_from_max.pack()
+
+label_title_to_webglviewer = Label(window, text="Chemin d'accès vers le dossier model du webgl viewer")
+# label_title_to_webglviewer.pack()
+
+label_title_to_save_directory = Label(window, text="Chemin d'accès vers le dossier de sauvegarde du zip")
+# label_title_to_save_directory.pack()
+
+
+
+# afficher la fenetre
+window.mainloop()
+
+
+
